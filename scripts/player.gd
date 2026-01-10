a33c70efd7c8dbd347b41957e6a3f6821f1c5042
Jason Liu <injadude01@gmail.com>
2026-01-09 20:57:55 -0500

    new game
    redid game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -850.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	#Add animation
+	if velocity.x > 1 or velocity.x < -1:
+		animated_sprite_2d.animation = "running"
+		
+	#Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -15,7 +21,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
