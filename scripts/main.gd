fa6e19215f0914c600c341413b48aefa7110eee6
Jason Liu <injadude01@gmail.com>
2026-01-29 21:51:37 -0500

    connected levels together
    connected levels 1 and 2, added assets, improved level1

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,38 +1,61 @@
 extends Node2D
 
 var score: int = 0
+var level: int = 1
+var current_level_root: Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_setup_level()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-func _setup_level() -> void:
 	
-	#connect enemys
-	var enemies = $Levelroot.get_node_or_null("Enemies")
+	#Setup the level
+	current_level_root = get_node("LevelRoot")
+	_load_level(level)
+
+# Level Management
+func _load_level(level_number: int) -> void:
+	if current_level_root:
+		current_level_root.queue_free()
+	
+	# Change level
+	var level_path = "res://scenes/levels/level%s.tscn" % level_number
+	current_level_root = load(level_path).instantiate()
+	add_child(current_level_root)
+	current_level_root.name = "Levelroot"
+	_setup_level(current_level_root)
+
+func _setup_level(level_root: Node) -> void:
+	
+	# connect Exit
+	var exit = level_root.get_node_or_null("Exit")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
+	
+	# Connect enemys
+	var enemies = level_root.get_node_or_null("Enemies")
 	if enemies:
 		for enemy in enemies.get_children():
 			enemy.player_died.connect(_on_player_died)
 	
-	#connect orbs
-	var orbs = $Levelroot.get_node_or_null("Orbs")
+	# Connect orbs
+	var orbs = level_root.get_node_or_null("Orbs")
 	if orbs:
 		for orb in orbs.get_children():
 			orb.collected.connect(increase_score)
 
 
-#---signals---
-
-	#death
+#---Signals---
+func _on_exit_body_entered(body : Node2D) -> void:
+	if body.name == "Player":
+		level+=1
+		body.can_move = false
+		call_deferred("_load_level" ,level)
+	
+	#Death
 func _on_player_died(body):
 	print("you died lol")
 	body.die()
 
-	#score#
+	#Score#
 func increase_score() -> void:
 	score += 1
 	print(score)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 const SPEED = 450.0
 const JUMP_VELOCITY = -1000.0
 var alive = true
-
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	
@@ -22,24 +22,25 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jumping"
+	
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("Up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("Left", "Right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
-	move_and_slide()
-
-	if direction == 1.0: 
-		animated_sprite_2d.flip_h = true
-	elif direction == -1.0: 
-		animated_sprite_2d.flip_h = false
+		if direction == 1.0: 
+			animated_sprite_2d.flip_h = true
+		elif direction == -1.0: 
+			animated_sprite_2d.flip_h = false
 		
 		
 func die() -> void:
